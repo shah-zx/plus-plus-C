96457dc12f6e7dd4a67a44ee49d16bcfebd83e77
shah-zx <shahnawazsayyed456@gmail.com>
2021-11-24 02:20:08 +0530

    Done with star patterns

---
+++ .history/Data-structures/stars_pattern_20211124020543.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row , col;
+    cout<<"Please enter the number of rows"
+}
---
+++ .history/Data-structures/stars_pattern_20211124020809.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row , col;
+    cout<<"Please enter the number of rows"<<endl;
+    cin>>row;
+    cout<<"Please enter the number of columns"<<endl;
+    cin>>col;
+    for (int i = 0; i <= row; i++)
+    {
+        for (int i = 0; i <= col; i++)
+        {
+            cout<<"*"
+        }
+        
+    }
+    
+
+}
