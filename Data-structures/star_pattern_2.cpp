72f8c3039e81bdcd7e5e34cef4e267df2e0aa227
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-01 00:12:29 +0530

    Added

---
+++ Data-structures/star_pattern_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row, col;
+    cout << "Please enter the number of rows" << endl;
+    cin >> row;
+    cout << "Please enter the number of columns" << endl;
+    cin >> col;
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            if (i ==1 || i == row)
+            {
+                cout<<"*";
+            }
+            else if (j == 1 || j == row)
+            {
+                cout<<"*";
+            }
+            
+        }
+        cout << endl;
+    }
+}
