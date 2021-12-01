189c4384b32dbd904a9526a0f91c6d2901023a1c
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-01 22:00:49 +0530

    Done with reverse of a number

---
+++ .history/Data-structures/Reverseornot_20211201205944.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;  
+    cout<<"Enter the number to be reversed";
+    cin>>num;
+    int reverse = 0;
+    while (num>0)
+    {
+        int lastdigit;
+        reverse += lastdigit;
+    }
+    
+         
+}
---
+++ .history/Data-structures/Reverseornot_20211201213151.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;  
+    cout<<"Enter the number to be reversed";
+    cin>>num;
+    int reverse = 0;
+    while (num>0)
+    {
+        int lastdigit;
+        reverse = reverse*10 + lastdigit;
+    }
+            
+}
