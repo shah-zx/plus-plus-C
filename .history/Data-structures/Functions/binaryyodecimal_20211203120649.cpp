a856edd1e49a307f174e8e9058a42eea1d874d8c
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-03 12:09:05 +0530

    Done with binary to decimal

---
+++ .history/Data-structures/Functions/binaryyodecimal_20211203120649.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int binaryToDecimal(int num)
+{
+    int ans = 0;
+    int x = 1;
+    while (num > 0)
+    {
+
+        int lastdigit = num % 10;
+        ans += x * lastdigit;
+        x *= 2;
+        num /= 10;
+    }
+}
+
+int main()
+{
+    int num;
+    cout << "Enter the number";
+    cin >> num;
+    cout << "The decimal of the given number is :" << binaryToDecimal(num);
+}
