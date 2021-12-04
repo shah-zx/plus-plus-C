d8aaa08b6e804a915f69d15b899740426bc8665b
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-04 15:15:20 +0530

    Done with finding maximum element in the array

---
+++ .history/Data-structures/Arrays/Maxinarray_20211204151403.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of the array :";
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+    int mx = 0;
+    for (int i = 0; i < n; i++)
+    {
+        mx = max(mx, array[i]);
+    }
+    cout << mx << endl;
+}
