f20e5a4d3c5977fd339035a7de0095ad69b6ad6c
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-07 01:49:40 +0530

    Done with multiplication of arrays

---
+++ .history/Data-structures/Arrays/Multiplicationofarrays_20211207005017.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    cout<<"Enter the number of rows and columns:";
+    int n1 , n2 , n3;
+    int m[n1][n2];
+    int n[n2][n3];
+    
+
+}
