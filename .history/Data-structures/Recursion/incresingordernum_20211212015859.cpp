7d01bb5c28c6ce87e26344925db858591e1d7ba4
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-12 02:13:54 +0530

    Done with more recursions

---
+++ .history/Data-structures/Recursion/incresingordernum_20211212015859.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int Inc(int n)
+{
+    if (n == 1)
+    {
+        cout << 1 << endl;
+    }
+    Inc(n - 1);
+    cout << n << endl;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << Inc(num);
+}
---
+++ .history/Data-structures/Recursion/incresingordernum_20211212020047.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int Inc(int n)
+{
+    if (n == 1)
+    {
+        cout<<"1"<<endl;
+        return 1;
+    }
+    Inc(n - 1);
+    cout << n << endl;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << Inc(num);
+}
