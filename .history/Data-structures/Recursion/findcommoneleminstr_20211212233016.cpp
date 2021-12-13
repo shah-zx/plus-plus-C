370c297282afbc70ee38b0a33feafcbb21c74255
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-13 13:30:40 +0530

    Done with moveallx and find common element problem

---
+++ .history/Data-structures/Recursion/findcommoneleminstr_20211212233016.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+string Finddup(string s)
+{
+    if (s.length()==0)
+    {
+        return;
+    }
+    if (s[0]==s[1])
+    {
+        
+    }
+    string ros = Finddup(s.substr(1));
+
+    
+}
+
+int main(){
+
+}
---
+++ .history/Data-structures/Recursion/findcommoneleminstr_20211212233320.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+string Finddup(string s)
+{
+    if (s.length()==0)
+    {
+        return "";
+    }
+    string ros = Finddup(s.substr(1));
+    if (s[0]==ros[0])
+    {
+        ros;
+    }
+    return s[0]+ ros;
+
+    
+}
+
+int main(){
+
+}
