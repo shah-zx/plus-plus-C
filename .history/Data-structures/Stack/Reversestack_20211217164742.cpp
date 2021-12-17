56ef3b1fb933316efaeca1040988eb9328dea577
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-17 17:24:55 +0530

    Done with reversing the stack

---
+++ .history/Data-structures/Stack/Reversestack_20211217164742.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+void InsertAtBottom(stack <int> &st , int ele)
+{
+   int element = st.top();
+   st.pop();
+   InsertAtBottom(st , ele);
+   st.push(ele);
+}
+
+
+
+void Reverse(stack <int>)
+{
+    stack<int> st;
+    int ele = st.top();
+    Reverse(st);
+
+}
+
+
+int main()
+{}
