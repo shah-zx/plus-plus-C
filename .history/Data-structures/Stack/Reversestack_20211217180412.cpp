088127412394702f027382e4f49bdbd2ffd78c03
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-17 18:43:26 +0530

    Done with revering the array

---
+++ .history/Data-structures/Stack/Reversestack_20211217180412.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void InsertAtBottom(stack<int> &st, int ele)
+{
+    if (st.empty())
+    {
+        st.push(ele);
+        return;
+    }
+
+    int element = st.top();
+    st.pop();
+    InsertAtBottom(st, ele);
+    st.push(element);
+}
+
+void Reverse(stack<int> &st)
+{
+    if (st.empty())
+    {
+        return;
+    }
+
+    int ele = st.top();
+    st.pop();
+    Reverse(st);
+    InsertAtBottom(st, ele);
+}
+
+int main()
+{
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+    Reverse(st);
+    while (!st.empty())
+    {
+        cout << st.top()<< endl;
+        st.pop();
+    }
+    cout << endl;
+}
