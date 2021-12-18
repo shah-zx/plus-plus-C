a3c4294053b05ab8a2824d40126758ea72513a13
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-18 15:11:15 +0530

    Done with unbalanced parantheses

---
+++ .history/Data-structures/Stack/Unbalancedparantheses_20211218145348.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+bool isValid(string s)
+{
+     int l = s.length();
+     for (int i = 0; i < l; i++)
+     {
+         stack <char> st;
+         if (s[i] == '{' or s[i] == '(' or s[i] == '[')
+         {
+             
+         }
+         
+     }
+     
+}
+
+
+int main()
+{
+   string s = "{([])}";
+
+}
