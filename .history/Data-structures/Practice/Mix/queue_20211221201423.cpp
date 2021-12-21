6f73644412d48d1bc3427570b8e2f94b482af030
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-21 20:35:49 +0530

    Added

---
+++ .history/Data-structures/Practice/Mix/queue_20211221201423.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+using namespace std;
+
+
+#define n 10
+
+class queue{
+    int *arr; 
+    int front;
+    int back;
+    queue()
+    {
+        front = -1;
+        back = -1;
+    }
+}
+
+
+
+
+int main(){
+
+}
