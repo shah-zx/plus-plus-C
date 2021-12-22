385ffa38d3f7fcdbda5afa7ebba64ab73bffe9e0
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-22 15:27:44 +0530

    Added

---
+++ .history/Data-structures/Practice/Mix/lowestcommonancestor_20211222145203.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+using namespace std;
+
+
+struct Node {
+    int data;
+    Node * left;
+    Node* right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+}
+
+
+int LCA(Node * root , int n1 , int n2)
+{
+
+}
+
+
+int main()
+{
+
+}
