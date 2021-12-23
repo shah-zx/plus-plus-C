981760a77a3ba1388ec606a4e4d8e4c31c782236
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-23 23:03:59 +0530

    Done      InsertBST(root ,1);

---
+++ .history/Data-structures/Binary search tree/bst_20211223175758.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+struct Node{
+    int data;
+    Node * left;
+    Node * right;
+    Node(int val)
+    {
+        data = val;
+        lef= NULL;
+        right= NULL;
+    }
+}
+
+
+int main()
+{
+
+}
---
+++ .history/Data-structures/Binary search tree/bst_20211223193219.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node *InsertBST(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        return new Node(val);
+    }
+    if (val < root->data)
+    {
+        root->left = InsertBST(root->left, val);
+    }
+    else // If the value is greater than the root ka data
+    {
+        root->right = InsertBST(root->right, val);
+    }
+    return root;
+}
+
+int main()
+{
+
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+}
