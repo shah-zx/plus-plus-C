3c4978d1f511ac663f60a501f009b1502418e55a
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-26 22:28:34 +0530

    Doing practice

---
+++ .history/Data-structures/Practice/Mix/Automaticvendingmachine_20211226221713.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+//     Coffee
+
+// 1. Espresso Coffee
+// 2. Cappuccino Coffee
+// 3. Latte Coffee
+
+//  Tea
+// 1. Plain Tea
+// 2. Assam Tea
+// 3. Ginger Tea
+// 4. Cardamom Tea
+// 5. Masala Tea
+// 6. Lemon Tea
+// 7. Green Tea
+// 8. Organic Darjeeling Tea
+// Soups 
+
+// 1. Hot and Sour Soup
+// 2. Veg Corn Soup
+// 3. Tomato Soup
+// 4. Spicy Tomato Soup
+// 5. Beverages
+
+// 1. Hot Chocolate Drink
+// 2. Badam Drink
+// 3. Badam-Pista Drink
+
+char drink;
+cin>>drink;
+int option;
+cin>>option;
+switch (drink)
+{
+    case 'c' :
+    cout<<"Welcome to CCD"
+}
+
+
+}
---
+++ .history/Project/Automaticvendingmachine_20211226222809.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    //     Coffee
+
+    // 1. Espresso Coffee
+    // 2. Cappuccino Coffee
+    // 3. Latte Coffee
+
+    //  Tea
+    // 1. Plain Tea
+    // 2. Assam Tea
+    // 3. Ginger Tea
+    // 4. Cardamom Tea
+    // 5. Masala Tea
+    // 6. Lemon Tea
+    // 7. Green Tea
+    // 8. Organic Darjeeling Tea
+    // Soups
+
+    // 1. Hot and Sour Soup
+    // 2. Veg Corn Soup
+    // 3. Tomato Soup
+    // 4. Spicy Tomato Soup
+    // 5. Beverages
+
+    // 1. Hot Chocolate Drink
+    // 2. Badam Drink
+    // 3. Badam-Pista Drink
+
+    char drink;
+    cin >> drink;
+    int option;
+    cin >> option;
+    if (drink == 'c' && option == 1)
+    {
+        cout << "Welcome to CCD";
+        cout << "Enjoy your Espresso Coffee!";
+    }
+    else if (drink == 'c' && option == 2)
+    {
+        cout << "Welcome to CCD";
+        cout << "Enjoy your Espresso Coffee!";
+    }
+    else if (drink == 'c' && option == 3)
+    {
+        cout << "Welcome to CCD";
+        cout << "Enjoy your Espresso Coffee!";
+    }
+    if (drink == 't' && option == 1)
+    {
+        cout << "Welcome to CCD";
+        cout << "Enjoy your Espresso Coffee!";
+    }
+}
