9614f0e3c38df97195ddec6dbcf404d771f62559
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-26 13:53:34 +0530

    Done with catalan sequence

---
+++ .history/Data-structures/catalan numbers/catalanseq_20211226130218.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int Catalan(int n)
+{
+
+}
+
+
+
+
+int main()
+{
+
+}
---
+++ .history/Data-structures/catalan numbers/catalanseq_20211226131036.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int Catalan(int n)
+{
+
+    int res = 0;
+    if (n <= 1)
+    {
+        return 1;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        res += Catalan(i) * Catalan(n - i - 1);
+    }
+    return res;
+}
+
+int main()
+{
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << Catalan(i) << " ";
+    }
+    cout << endl;
+    return 0;
+}
