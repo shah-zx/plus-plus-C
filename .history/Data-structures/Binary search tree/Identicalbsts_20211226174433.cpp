28bcb8a56264d89ea82f622fdf5f5c23b90fd480
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-26 17:53:39 +0530

    Doing identical trees

---
+++ .history/Data-structures/Binary search tree/Identicalbsts_20211226174433.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+struct Node{
+    int data;
+    Node * left;
+    Node * right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+bool isIdentical(Node * root1 , Node * root2)
+{
+    if()
+}
+
+
+
+
+
+int main()
+{
+
+}
