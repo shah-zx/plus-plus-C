078b30e64b8287752a7018442cfad57b4576dadc
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-26 12:37:37 +0530

    Added

---
+++ .history/Data-structures/Practice/Mix/monkeys_20211225232335.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+//  n= Total no of Monkeys
+// k= Number of eatable Bananas by Single Monkey (Monkey that jumped down last may get less than k Bananas)
+// j = Number of eatable Peanuts by single Monkey(Monkey that jumped down last may get less than j Peanuts)
+// m = Total number of Bananas
+// p  = Total number of Peanuts
+
+int main()
+{
+
+    int n;
+    int k;
+    int j;
+    int m;
+    int p;
+
+    cin >> n >> k >> j >> m >> p;
+    int rem = n - (m / k + p / j);
+    cout << "Number of  Monkeys left on the tree: " << rem << endl;
+}
