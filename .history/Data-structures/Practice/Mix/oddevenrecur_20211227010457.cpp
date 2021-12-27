8917e14f0dd63125ca06b35ce8e492c4ba3151fd
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-27 15:28:31 +0530

    Added

---
+++ .history/Data-structures/Practice/Mix/oddevenrecur_20211227010457.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int EveOdd(int n)
+{
+    if(n==0){
+        return 0;
+    }
+    int res = 0;
+    if (n % 2 == 0)
+    {
+        res += EveOdd(n + 6);
+    }
+    else
+    {
+        res += EveOdd(n + 7);
+    }
+    return res;
+}
+
+int main()
+{
+
+}
