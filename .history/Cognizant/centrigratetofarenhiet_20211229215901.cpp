fc1f682f07ca3a328946e1729b1bb0ea7d43110b
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-29 22:06:55 +0530

    Doing practice

---
+++ .history/Cognizant/centrigratetofarenhiet_20211229215901.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+#define vi vector<int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+float Celtofar(float c)
+{
+    // To convert temperatures in degrees Celsius to Fahrenheit, multiply by 1.8 (or 9/5) and add 32.
+
+    int result = (c * 1.8) + 32;
+    cout << result;
+}
+
+int_fast32_t main()
+{
+  float c;
+  cin>>c;
+  
+
+
+}
