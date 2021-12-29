0113d2194a53f5dcf232c46912f581ceb003ba62
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-29 16:12:35 +0530

    Added

---
+++ .history/Data-structures/Heaps/medianofrunningstreams_20211229161107.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+#define vi vector<int>
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+
+
+priority_queue<int, vector<int>> pqmax;
+priority_queue<int, vector<int> ,greater <int>> pqmin;
+
+void Insert(int x)
+{
+    if (pqmax.size() == 0)
+    {
+        pqmax.push(x);
+    }
+    if (x<pqmax.top())
+    {
+        pqmax.push(x);
+    }
+    else{
+        pqmin.push(x);
+    }
+    
+}
+
+
+int_fast32_t main(){
+      
+ }
