4bf6bea84ad396a592351450e7bfc0622f3cee91
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-29 23:21:44 +0530

    Done with swapping without extra var

---
+++ .history/Cognizant/swapvalueswithoutthirdnum_20211229232000.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+#define vi vector<int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+int swap(int a, int b)
+{
+    a = a + b; // If a = 10 , b = 11
+    // Now a is 21
+    b = a - b; // Now b is 10 as 21 - 11 = 10
+    a = a - b; // Now a is 11 as 21 - 10 = 11
+    // Thus the values are swapped
+    cout << "a : " << a << "b : " << b << endl;
+}
+
+int_fast32_t main()
+{
+
+int a = 10;
+int b = 11;
+cout<<swap(a,b)
+}
