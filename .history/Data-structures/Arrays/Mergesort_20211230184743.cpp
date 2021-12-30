d43d9328421625404da7703acbf49336de777b3b
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-30 18:49:13 +0530

    Doing merge sort

---
+++ .history/Data-structures/Arrays/Mergesort_20211230184743.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+#define vi vector<int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+void merge(int arr[], int mid, int l, int r)
+{
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+
+    int a[n1];
+    int b[n2];
+
+    for (int i = 0; i < n1; i++) // This will be loop for first sorted array
+    {
+        a[i] = arr[l + i];
+    }
+
+    for (int i = 0; i < n2; i++) // This will be loop for second sorted array
+    {
+        b[i] = arr[mid + 1 + i];
+    }
+
+    // Here we are checking the conditions for smaller and larger element and putting them into the array //
+
+    int k = l;
+    int i = 0;
+    int j = 0;
+
+    while (i < n1 && j < n2)
+    {
+        if (arr[i] < arr[j])
+        {
+            arr[k] = arr[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            arr[k] = arr[j];
+            j++;
+            k++;
+        }
+    }
+
+    while (i<n1)
+    {
+        arr[k] = arr[i];
+        k++;
+        i++;
+    }
+    while (j<n2)
+    {
+       arr[k] = arr[j];
+       k++;
+       j++;
+    }
+    
+    
+
+}
+
+void mergesort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int mid = (l + r) / 2;
+        mergesort(arr, l, mid - 1);
+        mergesort(arr, mid + 1, r);
+        merge(arr, mid, l, r);
+    }
+}
+
+int_fast32_t main()
+{
+}
+
+// I can code whole the long and my passion is to code all the day
+// Last but not the least , i want a big company and a seven figures salry //
