668042a8d876a29b4f5d96ee1b6e960357802de2
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-31 21:49:56 +0530

    Added

---
+++ .history/Cognizant/prime_20211231213046.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+#define vi vector<int>
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+bool primeorNot(int n)
+{
+    for (int i = 0; i < ; i++)
+    {
+        /* code */
+    }
+    
+}
+
+
+int_fast32_t main(){
+
+
+
+ }
---
+++ .history/Cognizant/prime_20211231213647.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+#define vi vector<int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+void primeorNot(int n)
+{
+    for (int i = 0; i <=sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "Not prime" << endl;
+        }
+        else
+        {
+            cout<<"prime";
+        }
+    }
+}
+
+int_fast32_t main()
+{
+    int n;
+    cin>> n;
+    primeorNot(n);
+}
