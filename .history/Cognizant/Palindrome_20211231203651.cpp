921c6eced40e803bd87897e3edf5b00d673870a4
shah-zx <shahnawazsayyed456@gmail.com>
2021-12-31 21:21:08 +0530

    Doing practice

---
+++ .history/Cognizant/Palindrome_20211231203651.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+#define vi vector<int>
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+
+bool isPalindrome(int n)
+{
+    int num = n;
+    int res = 0;
+    while(n!=0)
+    {
+        int lastdigit = n % 10;
+        res += lastdigit * 10;
+        n = n/10;
+    }
+}
+
+
+int_fast32_t main(){ 
+
+
+
+
+}
---
+++ .history/Cognizant/Palindrome_20211231203726.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+#define vi vector<int>
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+
+bool isPalindrome(int n)
+{
+    int num = n;
+    int res = 0;
+    while(n!=0)
+    {
+        int lastdigit = n % 10;
+        res += lastdigit * 10;
+        n = n/10;
+    }
+    if(res == num)
+    {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+int_fast32_t main(){ 
+
+
+
+
+}
