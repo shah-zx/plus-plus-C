8e7dda6f7372e4c210548f7184f77b0238cd97fe
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-01 17:28:26 +0530

    Added

---
+++ .history/Data-structures/Greedy algo/fractionalknapsack_20220101172038.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+#define vi vector<int>
+#define vector <pair<int,int>>
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+
+void Units()
+{
+
+}
+
+
+int_fast32_t main(){
+
+int n;
+cin>>n;
+vector <pair<int,int> > a(n);
+rep(i,0,n)
+{
+  cin>>a[i].first>>a[i].second;
+}
+
+ }
---
+++ .history/Data-structures/Greedy algo/fractionalknapsack_20220101172406.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+  double v1 = a[i].first/
+
+}
+
+int_fast32_t main()
+{
+
+    int n;
+    cin >> n;
+    vii a(n);
+    rep(i, 0, n)
+    {
+        cin >> a[i].first >> a[i].second;
+    }
+    sort(a.begin(), a.end() , compare);
+}
