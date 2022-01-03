bff7200a5e2047ef8b9bf3cef807d821fde11670
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-03 16:14:05 +0530

    Doing pactice[F

---
+++ .history/Cognizant/searchin2darray_20220103130418.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main(){
+
+
+    int m;
+    cin>>m;
+    int n;
+    cin>>n;
+    int arr[m][n]   = {};
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin>>arr[i][j];
+        }
+        
+    }
+    
+}
