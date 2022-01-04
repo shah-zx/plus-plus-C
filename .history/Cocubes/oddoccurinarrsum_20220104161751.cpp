5ed18f859c08b3255b22d597c20cb6015b654c8f
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-04 16:40:04 +0530

    Added

---
+++ .history/Cocubes/oddoccurinarrsum_20220104161751.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int n;
+    cin >> n;
+    vi a(n);
+
+    rep(i, 0, n)
+    {
+        cin >> a[i];
+    }
+
+    map<int, int> p;
+    for (int i = 0; i < n; i++)
+    {
+        p[a[i]]++;
+    }
+
+    map<int, int>::iterator it;
+    for (it = p.begin(); it != p.end(); it++)
+    {
+        cout<<it->first <<" "<<it->second<<endl;
+    }
+}
---
+++ .history/Cocubes/sumofdigits_20220104163809.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+int sum(int n)
+{
+    int Sum = 0;
+    rep(i, 0, n)
+    {
+        Sum += i;
+    }
+    cout<<Sum;
+}
+
+signed main()
+{
+}
