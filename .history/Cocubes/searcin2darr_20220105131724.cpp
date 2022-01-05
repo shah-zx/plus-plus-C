7bbc54083d4bdb374c9d05ffcb7981202ce18b56
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-05 13:17:59 +0530

    Added

---
+++ .history/Cocubes/searcin2darr_20220105131724.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_set>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+signed main()
+{
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int num;
+    cin >> num;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j] == num)
+            {
+                cout << "(" << i << "," << j << ")";
+            }
+        }
+    }
+    cout<<"(-1,-1)";
+}
