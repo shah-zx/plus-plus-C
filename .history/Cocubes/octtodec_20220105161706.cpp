0fd6105947be8f2d498af39f4aee11f6ed010100
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-05 17:06:30 +0530

    Added

---
+++ .history/Cocubes/octtodec_20220105161706.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_set>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int n;
+    cin >> n;
+    int sum = 0;
+    int i = 0;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        sum += digit * pow(8, i);
+        n = n/10;
+        i++;
+    }
+    cout<<sum << endl;
+}
