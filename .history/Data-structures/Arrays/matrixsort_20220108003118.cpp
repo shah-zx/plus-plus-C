9b8d858f6a01ab6bae11011cd66b17765a39b708
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-08 01:14:52 +0530

    Done

---
+++ .history/Data-structures/Arrays/matrixsort_20220108003118.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int m;
+    cin >> m;
+    int n;
+    cin >> n;
+    int arr[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 1; j < n-1; j++)
+        {
+            if (arr[i][j] < arr[i + 1][j + 1])
+            {
+                swap(arr[i][j], arr[i + 1][j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+    }
+}
