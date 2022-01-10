f05afa25e1d59eba1236a8620cb4f7f35e151698
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-10 10:15:47 +0530

    Done

---
+++ .history/Data-structures/Practice/Leetcode/deletioninarray_20220109231015.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    unordered_map<int, int> mp;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int target;
+    cin >> target;
+    for (int i = 0; i < n; i++)
+    {
+        mp[arr[i]]++;
+    }
+    for (auto x : mp)
+    {
+        cout<<x.first;
+    }
+}
