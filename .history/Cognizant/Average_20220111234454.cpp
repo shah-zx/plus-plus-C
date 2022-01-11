8cd464730bb3fcb0281b0d204cd01fa43c793b70
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-12 00:25:47 +0530

    Added

---
+++ .history/Cognizant/Average_20220111234454.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+signed main()
+{
+    int num;
+    cin >> num;
+    int sum = 0;
+    int arr[num] = {};
+    rep(i, 0, num)
+    {
+        cin >> arr[i];
+    }
+    rep(i, 0, num)
+    {
+        sum += arr[i];
+    }
+    int avg = sum / num;
+    cout << "Average is: " << avg << endl;
+}
