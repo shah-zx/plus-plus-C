3bcc58cd26fa6abf23bb509f5be57b182e1ea4df
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-17 17:22:16 +0530

    Done

---
+++ .history/codechef/inttostring_20220116221137.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int n = 2345;
+    ostringstream str1;
+    str1 << n;
+    string s;
+    s = str1.str();
+    cout<<s[0];
+
+}
