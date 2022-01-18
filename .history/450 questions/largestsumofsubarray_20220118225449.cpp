cc1cb5e10ce81d70a58f22d8d1393880465103e7
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-18 23:01:51 +0530

    Added

---
+++ .history/450 questions/largestsumofsubarray_20220118225449.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int max = 0;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int current = 0;
+        for (int j = 0; j < n; j++)
+        {
+            current += arr[j];
+        } 
+    }
+}
