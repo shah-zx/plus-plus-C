c0eb65a4df52b980f514c7ea33fcc694f5534538
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-21 21:29:45 +0530

    Doing practice

---
+++ .history/codechef/puppyandsum_20220121205403.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+int Sum(int n){
+    int s = 0;
+    if(n==1){
+        return 1;
+    } s += n + Sum(n-1);
+    
+}
+
+signed main(){
+
+int T;
+cin>>T;
+for (int i = 0; i < T; i++)
+{
+    int n1 , n2;
+    cin>>n1>>n2;
+
+
+}
+
+
+}
