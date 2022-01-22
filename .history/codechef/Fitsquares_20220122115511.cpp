017440d8c32df87d1128d4b1ca1792be56468da9
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-22 14:14:03 +0530

    Doing practice

---
+++ .history/codechef/Fitsquares_20220122115511.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n; // base of triangle
+        cin >> n;
+        int squares = 0;
+        while (n > 2)
+        {
+            squares += (n - 2) / 2;
+            n -= 2;
+        }
+        cout << squares << endl;
+    }
+}
---
+++ .history/codechef/sumpair_20220122140434.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main(){
+
+int t;
+cin>>t;
+while(t--)
+{
+    int n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+        for (int i = 0; i < n-1; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                /* code */
+            }
+            
+        }
+        
+    }
+    
+}
+
+
+}
