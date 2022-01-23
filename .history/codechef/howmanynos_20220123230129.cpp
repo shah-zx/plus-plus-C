6b626710d33313b501bdba5e5ec012ebf189dc61
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-23 23:22:02 +0530

    Added

---
+++ .history/codechef/howmanynos_20220123230129.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+    int i;
+    cin>> i;
+    int count = 0;
+    while (i != 0)
+    {
+        int ld = i % 10;
+        count ++;
+        i = i / 10;
+    } if ( count > 3){
+        cout << "More than 3 digits" << endl;
+    }
+    
+}
