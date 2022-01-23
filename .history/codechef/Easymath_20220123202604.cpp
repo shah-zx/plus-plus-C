ce943205a1b154419352ca1c135fd18cd2d5ae05
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-23 20:53:58 +0530

    Added

---
+++ .history/codechef/Easymath_20220123202604.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int T = 0;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        int n;
+        int arr[n];
+        for (int j = 0; j < n; j++)
+        {
+            cin>>arr[j];
+        }
+        
+    }
+    
+}
