124b8b295d27ce0a8e85c782a35e9e0f74a5d229
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-26 17:44:33 +0530

    Added

---
+++ .history/codechef/twonos_20220126171054.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+
+bool Even(int n){
+    if(n % 2 == 0) return true;
+}
+
+signed main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+
+        int a, b, t;
+        cin >> a >> b >> t;
+        for (int i = t; i > 0; i--)
+        {
+            if()
+        }
+        
+    }
+}
---
+++ .history/codechef/twonos_20220126172331.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+bool Even(int n)
+{
+    if (n % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+signed main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+
+        int a, b, t;
+        cin >> a >> b >> t;
+        int counta = 0;
+        int countb = 0;
+        for (int i = t; i > 0; i--)
+        {
+            if (!Even(i))
+            {
+                counta = a *= 2;
+            }
+            else
+            {
+                countb = b *= 2;
+            }
+        }
+        int y = max(counta, countb);
+        int z = min(counta, countb);
+        cout << y << endl;
+    }
+}
