dfb802a6eb05314c0ec2e890a3db49c4e9d5c890
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-27 15:45:14 +0530

    Added

---
+++ .history/Hackerrank/minimax_20220127150542.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    // int n;
+    // cin >> n;
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+    // sort(arr, arr + n);
+    // for (int i = 1; i < n; i++)
+    // {
+    //     cout << arr[i];
+    // }
+
+    int n;
+    cin >> n;
+    int arr[n];   
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int t;  
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = i + 1; j < 5; i++)
+        {
+            if (arr[j] < arr[i])
+            {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+        }
+    }
+    // 
+    for (int i = 0; i < 4; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
