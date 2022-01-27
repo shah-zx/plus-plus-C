9353c63cdaf2ebf407877dbbbdb8de0d0c22a69b
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-27 11:57:31 +0530

    Added

---
+++ .history/Hackerrank/cake_20220127112933.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+    int n;
+    int arr[n];
+    unordered_map<int, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    int g = arr[n-1];
+    for (int i = 0; i < n; i++)
+    {
+        int k;
+        m[k]++;
+    }
+    for (auto i : m)
+    {
+        if(i.first == g){
+            cout << i.second << end
+        }
+    }
+    
+}
