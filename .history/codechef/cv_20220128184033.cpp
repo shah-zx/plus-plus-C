d2f1caed0b26925a497f14a68fac41bf92bc6794
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-28 19:32:57 +0530

    Added

---
+++ .history/codechef/cv_20220128184033.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+bool VowelorNot(char c){
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+}
+
+signed main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin>>n;
+        char arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; i < n; i++)
+            {
+                
+            }
+            
+        }
+    }
+}
