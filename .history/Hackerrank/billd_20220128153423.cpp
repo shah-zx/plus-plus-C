0ae5a7bf57b2da3fa611775e5f02cad8f325fc79
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-28 15:40:38 +0530

    Added

---
+++ .history/Hackerrank/billd_20220128153423.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int b;
+    cin >> b;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    int y = sum;
+    for (int j = 0; j < n; j++)
+    {
+        if (j == k)
+        {
+            y - arr[j];
+        }
+    cout << y << endl;
+
+    }
+}
