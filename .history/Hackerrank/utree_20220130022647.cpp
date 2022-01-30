194102b47dcd40c29f089ddfd54512050a806b9c
shah-zx <shahnawazsayyed456@gmail.com>
2022-01-30 11:01:10 +0530

    Added

---
+++ .history/Hackerrank/utree_20220130022647.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+
+        int n;
+        cin >> n;
+        int sumo = 0;
+        int sume = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 1)
+            {
+                sume * 2;
+            }
+            else
+            {
+                sume + 1;
+            }
+        }
+        cout << sume << endl;
+    }
+}
---
+++ .history/Hackerrank/utree_20220130104704.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+
+        int n;
+        cin >> n;
+        int sumo = 0;
+        int sume = 0;
+        for (int i = 0; i < n; i++)
+        {
+           if(i % 2 == 0){
+               sume += 1;
+           }
+           else{
+               sumo = sume * 2;
+           }
+        }
+        cout << sumo << endl;
+    }
+}
