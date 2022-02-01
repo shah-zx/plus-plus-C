1fc83430d8df6f2e5c95e2b34fc2261bc768b94e
shah-zx <shahnawazsayyed456@gmail.com>
2022-02-01 14:12:34 +0530

    Added

---
+++ .history/Hackerrank/servicelane_20220201135046.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    vector<int> v;
+    int s, e;
+    int y = 0;
+    int h = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        cin >> s >> e;
+    }
+    // for (int i = 0; i < k; i++)
+    // {
+
+    //     for (int i = s; i <= e; i++)
+    //     {
+    //         v.push_back(arr[i]);
+    //     }
+    //     int g = *min_element(v.begin(), v.end());
+    //     cout << g << endl;
+    // }
+
+    for(int i  = s; i <= e; i ++){
+        cout << arr[i] << endl;
+    }
+
+    // s = y;
+    // e = h;
+}
