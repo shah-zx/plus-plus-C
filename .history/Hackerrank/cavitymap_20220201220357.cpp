dac6f6f101f35587593b4f4232f79801ff54bbc6
shah-zx <shahnawazsayyed456@gmail.com>
2022-02-01 23:15:01 +0530

    Added

---
+++ .history/Hackerrank/cavitymap_20220201220357.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int n;
+    cin >> n;
+    char arr[100][100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 1; i < n - 1; i++)
+    {
+        for (int j = 1; j < n - 1; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
+                cout << arr[i][j];
+            }
+            else if (arr[i][j] > arr[i][j - 1] || arr[i][j] > arr[i][j + 1] || arr[i][j] > arr[i + 1][j] || arr[i][j] > arr[i - 1][j]){
+                cout << "X";
+            }
+        }
+        cout << endl;
+    }
+}
---
+++ .history/Hackerrank/cavitymap_20220201220935.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int n;
+    cin >> n;
+    char arr[100][100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == n - 1 || j == 0 || j == n - 1)
+            {
+                cout << arr[i][j];
+            }
+            else if (arr[i][j] > arr[i][j - 1] && arr[i][j] > arr[i][j + 1] && arr[i][j] > arr[i + 1][j] && arr[i][j] > arr[i - 1][j])
+            {
+                cout << "X";
+            }
+            else
+            {
+                cout << arr[i][j];
+            }
+        }
+        cout << endl;
+    }
+}
