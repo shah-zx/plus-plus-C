a31367489c6e61c1ef70e35e00d9aea1a6ec4803
shah-zx <shahnawazsayyed456@gmail.com>
2022-02-02 17:00:26 +0530

    Added

---
+++ .history/Hackerrank/valley_20220202161426.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    string s;
+    int h = s.length();
+    for (int i = 0; i < h; i++)
+    {
+        cin >> s[i];
+    }
+    int count = 0;
+    for (int j = 0; j < h; j++)
+    {
+        if(s[j] == 'U'){
+          count++;
+        }
+        if(s[j] == 'D'){
+            count--;
+        }
+    }
+
+    
+}
---
+++ .history/Hackerrank/valley_20220202162249.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+    int h;
+    cin>>h;
+    vector <string> s;
+    int count = 0;
+    int rem = -1;
+    for (int j = 0; j < h; j++)
+    {
+        if (s[j] == 'U')
+        {
+            count++;
+        }
+        if (s[j] == 'D')
+        {
+            count--;
+        }
+        if (count == 0)
+        {
+            rem++;
+        }
+    }
+    cout << rem << endl;
+}
