70108dc556546f2d0fcab21cfcaa4da620bd97b1
shah-zx <shahnawazsayyed456@gmail.com>
2022-02-05 02:09:37 +0530

    Added

---
+++ .history/Data-structures/Sorting/Insertionsort_20220205015912.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+signed main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > current)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+        cout << arr[i] << endl;
+    }
+
+    
+}
