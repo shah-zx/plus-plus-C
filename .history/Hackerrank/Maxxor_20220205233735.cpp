588cb03a290aae04819a91de280d35c6434a44b0
shah-zx <shahnawazsayyed456@gmail.com>
2022-02-05 23:44:13 +0530

    Added

---
+++ .history/Hackerrank/Maxxor_20220205233735.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+
+    int a;
+    cin >> a;
+    int b;
+    cin>> b;
+    vector<int> v;
+    for (int i = a; i <= b; i++)
+    {
+        cin >> arr[i];
+    }
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            s = arr[j] ^ arr[i];
+            v.push_back(s);
+        }
+    }
+    int r = *max_element(v.begin(), v.end());
+    cout << r << endl;
+}
