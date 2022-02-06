a307d45aa095026df1436860e7f50b1da401ea64
shah-zx <shahnawazsayyed456@gmail.com>
2022-02-06 14:55:30 +0530

    Added

---
+++ .history/Hackerrank/twoCharacter_20220206135751.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+signed main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+
+        string s;
+        cin>>s;
+        int y = s.length();
+        unordered_map<char, int> m;
+        vector<int> v;
+        for (int i = 0; i < y; i++)
+        {
+            char c = s[i];
+            m[c]++;
+        }
+        for ( auto j : m)
+        {
+               
+        }
+        
+    }
+}
