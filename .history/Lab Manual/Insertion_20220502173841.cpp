30fa8cd3d4e4d341f53fec327484b54248d7bd40
shahnawaz sayyed <shahnawazsayyed456@gmail.com>
2022-05-02 17:56:11 +0530

    Added

---
+++ .history/Lab Manual/Insertion_20220502173841.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+
+
+signed main(){
+
+// Insertion sort  
+
+int n;
+cin>>n;
+int arr[n];
+for (int i = 0; i < n; i++)
+{
+    cin>>arr[i];
+}
+
+for (int i = 0; i < n; i++)
+{
+    int j = i - 1;
+    int current = arr[j];
+    while(j > 0 && arr[j] > current)
+    {
+        arr[j] = arr[j + 1];
+        j--;
+    }
+    arr[j + 1] = current;
+}
+
+
+
+
+
+}
