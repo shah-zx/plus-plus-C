ef5a29fc9213117aee57755d3d5bc8bdcb325650
shahnawaz sayyed <shahnawazsayyed456@gmail.com>
2022-05-03 23:46:54 +0530

    Added

---
+++ .history/Data-structures/Greedy algo/kanpsack_20220503232724.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const int N = 1e5 + 2;
+#define ll long long int
+ll dp[101][101];
+int wt[N], val[N];
+
+// int Knapsack(int n, int w)
+// {
+//     if (w <= 0)
+//     {
+//         return 0;
+//     }
+//     if (n <= 0)
+//     {
+//         return 0;
+//     }
+//     if (wt[n - 1] <= w)
+//     {
+//         return Knapsack(n - 1, w);
+//     }
+
+//     return max(Knapsack(n - 1, w), Knapsack(n - 1, w - wt[n - 1]) + val[n - 1]);
+// }
+
+ll knapsackR(ll wt[], ll val[], ll w, ll n)
+{
+    if (n == 0 || w == 0)
+        return 0;
+    else if (wt[n - 1] <= w)
+        return max(val[n - 1] + knapsackR(wt, val, w - wt[n - 1], n - 1), knapsackR(wt, val, w, n - 1));
+    return knapsackR(wt, val, w, n - 1);
+}
+
+signed main()
+{
+    ll n;
+    cin >> n;
+    ll wt[n], val[n], w;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> wt[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> val[i];
+    }
+
+    cin >> w;
+    ll ans = knapsackR(wt, val, w, n);
+    cout << ans;
+    return 0;
+}
---
+++ .history/Lab Manual/Knapsack_20220503233536.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+
+int KnapSack(int wt[] , int val[] , int n , int W){
+    if(n==0||wt==0)
+        return 0;
+    else if(wt[n-1]<= W){
+        return max(val[n-1] + Knapsack(wt[n]))
+    }
+}
+
+signed main(){
+
+}
